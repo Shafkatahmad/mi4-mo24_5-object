7e6bf9d48244f985c81196cc481049b305e82397
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-22 23:55:32 +0600

    array like objects

--- object.js
+++ object.js
@@ -2,5 +2,25 @@ const student = {
   name: 'Zohir Khan',
   id: 121,
   address: 'sport',
-  doesPlay: true
+  doesPlay: true,
+  friends: ['Apu', 'Rakib', 'Salman', 'Rakib'],
+  game: [{name: 'football', year: 2025}, {name: 'soccer', year: 2023}],
+  act: function() {
+    console.log('Playing like hero');
+  }
+  ,
+  car : {
+    brand: 'tesla',
+    price: 50000000,
+    made: 2025,
+    manufacturer: {
+      name: 'tesla',
+      ceo: 'elon mask',
+      country: 'usa'
+    }
+  }
 }
+
+// console.log(student);
+// console.log(student.act);
+student.act();
